c639d90e6a98e49f3b62e3f3f23a153f8946318c
Jonas Hagedorn <jhagedor@1-E-2.42heilbronn.de>
2022-03-08 16:03:14 +0100

    first draft Request Class

--- request/Request.hpp
+++ request/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: jhagedor <jhagedor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 13:16:53 by jhagedor          #+#    #+#             */
-/*   Updated: 2022/02/21 15:07:10 by jhagedor         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:41:48 by jhagedor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 # include <iostream>
 # include <map>
+# include <vector>
+
+
 
 class Request {
 	private:
@@ -26,9 +29,9 @@ class Request {
 		int									_port;
 
 	private:
-		/*** Parsing first line in request message ***/
-		int		readFirstLine(const std::string& line);
-		int		readMethod(const std::string& line, int i);
+		/* Parsing first line in request message */
+		int		parseFirstLine(const std::string& line);
+		int		readMethod(const std::string& line);
 		int		readPath(const std::string& line, int i);
 		int		readVersion(const std::string& line, int i);
 		
@@ -39,16 +42,20 @@ class Request {
 		Request(const std::string& str);
 		~Request();
 		
-		/*** Getter functions ***/
-		std::string							getMethod();
-		std::string							getPath();
-		std::string							getVersion();
-		std::map<std::string, std::string>	getHeaders();
-		std::string							getBody();
-		int									getRet();
-		int									getPort();
+		/* Getter functions */
+		std::string							getMethod() const;
+		std::string							getPath() const;
+		std::string							getVersion() const;
+		std::map<std::string, std::string>	getHeaders() const;
+		std::string							getBody() const;
+		int									getRet() const;
+		int									getPort() const;
 
-		int									parse(const std::string& str);
+		int									parse(const std::string &str);
+		std::string							nextLine(const std::string &str, size_t &i);
+		std::string							readKey(const std::string &str);
+		std::string							readValue(const std::string &str);
 
 		static std::vector<std::string>		initMethods();
 };
+
