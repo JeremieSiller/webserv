7b8cd4d19882fb92bbf69ae80ad080b3fd236e12
Jeremie Siller <jsiller@1-E-2.42heilbronn.de>
2022-03-09 14:13:51 +0100

    response class can now build the first line

--- response/response.hpp
+++ response/response.hpp
@@ -2,23 +2,26 @@
 # define RESPONSE_HPP
 
 #pragma once
+#include <string>
+#include <vector>
+#include <unistd.h>
+#define VERSION "HTTP/1.1"
 
 class response
 {
 private:
-	/* data */
+	const int					&_statsusCode;
+	const std::string			&_version;
+	std::vector<char>			_bytes;
+	void	_build();
+	void	_buildFirstLine();
+	void	_pushEndOfLine();
 public:
-	response(/* args */);
+	response(const int &status, const std::string &verison = VERSION);
 	~response();
+	int		write_response(const int &fd); //writes the full response to filedescriptor
+	void	add_header(const std::string &attribute, const std::string &value); 
+	void	add_header(const std::string &full_header);
 };
-
-response::response(/* args */)
-{
-}
-
-response::~response()
-{
-}
-
 
 #endif
---
+++ response/test.cpp
@@ -0,0 +1,6 @@
+#include "response.hpp"
+
+int	main(void) {
+	response	r(404);
+	r.write_response(1);
+}
